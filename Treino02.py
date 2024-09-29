3f76546f0364ce334182de945ac01a64f5263e50
Isabela <imcdsls10@gmailcom>
2024-09-28 21:11:38 -0300

    Outra página

---
+++ Treino02.py
@@ -0,0 +1,4 @@
+real = float(input('Quanto dinheiro você tem na sua conta bancária hoje? '))
+dolar = real / 5.48
+
+print('Com o valor {:.2f} na sua conta, você pode converter para {:.2f} US$'.format(real , dolar))
