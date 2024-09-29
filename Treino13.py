aef73ac07bb4200d8f5464a405ee56b62c33e010
Isabela <imcdsls10@gmailcom>
2024-09-29 17:15:19 -0300

    Mais um exercício

---
+++ Treino13.py
@@ -0,0 +1,4 @@
+valor = float(input('Para saber o valor do produto com desconto de 5%, digite a seguir o valor: '))
+des = valor - (valor * 5 / 100)
+print('O valor original do produto é {} '.format(valor))
+print('O valor total do produto com o desconto é {} '.format(des))
